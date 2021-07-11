eed9c883d95c0fd445d9987f6c14c1a571db1eb0
champii <37625021+champiix@users.noreply.github.com>
2021-07-11 18:50:34 +0300

    deleted useless things, recoded the help command

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import random
 from discord.ext import commands
+from discord.voice_client import VoiceClient
 import asyncio
 from webserver import keep_alive
 import os
@@ -8,6 +9,7 @@ import os
 client = commands.Bot(command_prefix = ".", guild_subscription = True)
 client.remove_command('help')
 format = "%a, %d %b %Y | %H:%M:%S %ZGMT"
+blacklist = ["410964165629575178", "241232014597029888"]
 
 @client.event
 async def on_ready():
@@ -84,10 +86,6 @@ async def unmute(ctx, member: discord.Member):
     await member.remove_roles(role)
     await ctx.send(f'{member.mention} unmuted')
 
-@client.command()
-async def pravda(ctx):
-    await ctx.send("Workers of the world unite!")
-
 @client.command(aliases=["zach", "zachary"])
 async def culprits(ctx):
   embed=discord.Embed(colour=0x1BFF00)
@@ -100,21 +98,9 @@ async def culprits(ctx):
   embed.set_image(url="https://res.cloudinary.com/du3fxrdqu/image/upload/v1614046027/peach%20bot/unknown-2_e3audi.png")
   await ctx.send(embed=embed)
 
-@client.command(pass_context=True, aliases=["propaganda"])
-async def manifesto(ctx):
-
-  embed = discord.Embed(
-    colour = discord.Colour.red()
-  )
-
-  embed.set_author(name="Pravda Manifesto Preface")
-  embed.add_field(name="uwu", value="The Pravda Union is a conglomeration of United Republics spanning from Eastern Europe and Northern Asia into Siberia. The works of the party have driven the parasites out of her borders and have continued to ensure safety within her beloved empire. The Central Committee and works have exceeded their mortal ambitions, and restored order and prosperity to the vast Union. Pravda stands as a turning point in history, about the enter into a state of hard work and productivity. To accomplish such a remarkable task, we must end the exploitation by the bourgeoisie, spread out socialist ideals and stand victorious over our foes. Workers of the world, unite! ☭")
-
-  await ctx.send(embed=embed)
-
 @client.command()
 async def info(ctx):
-    embed = discord.Embed(title="peach", description="AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", color=0xf4c2c2)
+    embed = discord.Embed(title="peach", description="cool bot i dont like it", color=0xf4c2c2)
 
     # give info about you here
     embed.add_field(name="Author", value="champii")
@@ -155,10 +141,6 @@ async def heal(ctx, member: discord.Member):
   await member.remove_roles(role)
   await ctx.send(f"{member.mention} got healed by "+ctx.message.author.mention)
 
-@client.command()
-async def t(ctx):
-  await ctx.send("stfu")
-
 @client.command(aliases=["n"])
 async def nordvpn(ctx):
   await ctx.send("Being on the internet can be very scary. With nord VPN, it can shield you from those harmful sites and safe guard your nudes you send to people like Insan. Be sure to use CODE pravda at checkout for a discount on the 3 yr plan which is only as low as 3.99 per month!")
@@ -186,8 +168,8 @@ async def slap(ctx, member : discord.Member):
   embed.set_image(url=f"{random.choice(responses)}")
   await ctx.send(f"{member.mention} got slapped by "+ctx.message.author.mention, embed=embed)
   
-@client.command(aliases=["simpdetector", "simp"])
-async def simprate(ctx,member : discord.Member):
+@client.command()
+async def simp(ctx,member : discord.Member):
   await ctx.send(f"{member.mention} is {random.randint(1,100)}% a simp.")
 
 @client.command(aliases=["miku"])
@@ -234,21 +216,21 @@ async def mochi(ctx):
   embed.set_image(url="https://res.cloudinary.com/du3fxrdqu/image/upload/v1613906874/peach%20bot/mEESSOZ_zznx5a.png")
   await ctx.send(embed=embed)
 
-@client.command(aliases=["gay"])
-async def gayrate(ctx,member : discord.Member):
+@client.command()
+async def gay(ctx,member : discord.Member):
   await ctx.send(f"{member.mention} is {random.randint(1,100)}% gay")
 
-@client.command(aliases=["overwatch"])
-async def ice(ctx):
+@client.command()
+async def overwatch(ctx):
   embed=discord.Embed(color=0xd7fffe)
   embed.set_image(url="https://media1.tenor.com/images/31e9558485e1c445420b81096d7c9f12/tenor.gif?itemid=7349756")
   await ctx.send(embed=embed)
 
-@client.command(aliases=["loli"])
-async def lolirate(ctx,member : discord.Member):
+@client.command()
+async def loli(ctx,member : discord.Member):
   await ctx.send(f"{member.mention} is {random.randint(1,100)}% loli")
 
-@client.command(aliases=["chii"])
+@client.command()
 async def champii(ctx):
  embed=discord.Embed()
  embed.set_image(url="https://res.cloudinary.com/du3fxrdqu/image/upload/v1613906967/peach%20bot/afpUzr5_kz3eon.jpg")
@@ -278,27 +260,14 @@ async def copypasta(ctx):
    "okay dude, that’s it, you’ve been acting a little gay lately and I’m genuinely concerned about your motives. Listen man, I honestly don’t care if you’re gay but when you do this gay ass shit to me it makes me feel extremely uncomfortable bro. I’m gonna ask you to stop right now",
    "What do you listen to dude? Rap? Bro, I'm only 12 and listen to kpop. You've probably never heard of it before, it's much too diverse for your liking. I'm not a fan of.. what's his name? Lilliam Pumpernickle? Yeaa, I can't stand that sort of music. Rap is always about big booty bitches and money and is far too mainstream. That's why I also listen to Jpop. It kinda lowkey and underground at the moment, but you should check out Twice, GFRIEND and IZ*one. Your rap can go take a hike while I listen to kpop and jpop with my friends. All of you think is that rap is superior and you wanna become a rapper. Well Kpop trainees take vigorous hours to be perfected meanwhile your stinky drug addict, alcoholic, horny rappers rap in some shitty bedroom that stinks of weed. Kpop idols have clean studios that are cleaned daily. Kpop music has a story meanwhile your stinky rap talks about bad stuff like drugs and alcohol. This is why kpop is superior and child friendly.",
    "Shut yo ching chong wing wong chila bila ping pong bling blong ring rong dinga dinga ding dong qing qong wing wong walla walla wing wong ying yong ying yang yingy yingy ying yang jing jong xing xong jinga xinga jing xang sing song sing sang sannga sannga sing sang iing iang ingy ingy iing iang ging gang ging ging ginga ginga ging gang hing haung hing hau hauie mauie hau hau ning nang ning nang ninga ningo ning ning zing zing zong zong zinga zingo zong zing ass the fuck up",
-   "I don't think you guys realize how cool I am. Unlike you peasants, I have Discord **NITRO!** Thats right, full nitro. What do you fucking idiots have? Classic? Get outta here. No Nitro? Fucking loser lol. None of you will **EVER** be on my level cause I have Nitro and you don't. Do you get animated profile pictures? Thats what I thought. Do you get ***TWO FREE SERVER BOOSTS?*** Thats what I thought. You also don't get to change your discrim! Thats right, the four numbers at the end of your username that you haven't payed ANY attention to. How does that make you feel, non-nitro?"]
+   "I don't think you guys realize how cool I am. Unlike you peasants, I have Discord **NITRO!** Thats right, full nitro. What do you fucking idiots have? Classic? Get outta here. No Nitro? Fucking loser lol. None of you will **EVER** be on my level cause I have Nitro and you don't. Do you get animated profile pictures? Thats what I thought. Do you get ***TWO FREE SERVER BOOSTS?*** Thats what I thought. You also don't get to change your discrim! Thats right, the four numbers at the end of your username that you haven't payed ANY attention to. How does that make you feel, non-nitro?"
+   ,"动态网自由门 天安門 天安门 法輪功 李洪志 Free Tibet 六四天安門事件 The Tiananmen Square protests of 1989 天安門大屠殺 The Tiananmen Square Massacre 反右派鬥爭 The Anti-Rightist Struggle 大躍進政策 The Great Leap Forward 文化大革命 The Great Proletarian Cultural Revolution 人權 Human Rights 民運 Democratization 自由 Freedom 獨立 Independence 多黨制 Multi-party system 台灣 臺灣 Taiwan Formosa 中華民國 Republic of China 西藏 土伯特 唐古特 Tibet 達賴喇嘛 Dalai Lama 法輪功 Falun Dafa 新疆維吾爾自治區 The Xinjiang Uyghur Autonomous Region 諾貝爾和平獎 Nobel Peace Prize 劉暁波 Liu Xiaobo 民主 言論 思想 反共 反革命 抗議 運動 騷亂 暴亂 騷擾 擾亂 抗暴 平反 維權 示威游行 李洪志 法輪大法 大法弟子 強制斷種 強制堕胎 民族淨化 人體實驗 肅清 胡耀邦 趙紫陽 魏京生 王丹 還政於民 和平演變 激流中國 北京之春 大紀元時報 九評論共産黨 獨裁 專制 壓制 統一 監視 鎮壓 迫害 侵略 掠奪 破壞 拷問 屠殺 活摘器官 誘拐 買賣人口 遊進 走私 毒品 賣淫 春畫 賭博 六合彩 天安門 天安门 法輪功 李洪志 Winnie the Pooh 劉曉波动态网自由门"]
   embed=discord.Embed(title="funny pasta", description=f"{random.choice(responses)}")
   await ctx.send(embed=embed)
 
-@client.command(aliases=["hornyrate"])
+@client.command()
 async def horny(ctx,member : discord.Member):
   await ctx.send(f"{member.mention} is {random.randint(1,100)}% horny")
-
-@client.command()
-@commands.has_permissions(kick_members=True)
-async def pmute(ctx, member: discord.Member):
-    role = discord.utils.get(ctx.guild.roles, name='Parasites')
-    await member.add_roles(role)
-    await ctx.send(f"{member.mention} was sent to the gulag.")
-
-@client.command()
-@commands.has_permissions(kick_members=True)
-async def punmute(ctx, member: discord.Member):
-    role = discord.utils.get(ctx.guild.roles, name='Parasites')
-    await member.remove_roles(role)
-    await ctx.send(f'{member.mention} was freed from the gulag by Western forces.')
 
 @client.command()
 async def kiss(ctx, member: discord.Member):
@@ -328,36 +297,20 @@ async def hug(ctx, member : discord.Member):
   await ctx.send(f"{member.mention} got a hug from "+ctx.message.author.mention, embed=embed)
 
 @client.command()
-async def help(ctx):
- embed=discord.Embed(title="Bot's code: https://github.com/champiix/bot", description="IMPORTANT\n``petition``\nFun\n``8ball``, ``coinflip|cf``, ``copypasta``, ``dice|roll``, ``horny|hornyrate``, ``gay|gayrate``, ``hug``, ``hackerman``, ``kill``,  ``kiss``,  ``lolirate``,  ``simp|simprate``,  ``slap``,``pat``, ``pedo|pedorate``,``sus``,``avatar``,``server``, ``liability``, ``lesbian``, ``filipino``\nModeration\n ``ban``,  ``mute``,  ``clear``,  ``kick``,  ``unmute``\nCustom Commands\n``alike``,  ``braindamage``,  ``champii``,  ``culprits``,  ``eesti``,  ``ice``, ``manifesto``,  ``mochi``,  ``miku``,  ``pravda``,  ``t``, ``snowy``,``zen``,``reece``,``xeno``,``unimech``\nVPN \n``pia``,  ``nordvpn``,  ``tunnelbear``\nBot Stuff\n``repo``,  ``ping``,  ``info``,  ``help (shows this message)``", color=0xF2AD7E)
- embed.set_author(name="Command list",icon_url="https://cdn.discordapp.com/avatars/692360784268754964/b82e34765cbeb04b2566e297b93197b0.png?size=2048")
- await ctx.send(embed=embed)
-
-@client.command()
 async def pat(ctx, member : discord.Member):
   pats = ["https://media1.tenor.com/images/da8f0e8dd1a7f7db5298bda9cc648a9a/tenor.gif?itemid=12018819", "https://media1.tenor.com/images/266e5f9bcb3f3aa87ba39526ee202476/tenor.gif?itemid=5518317", "https://media1.tenor.com/images/d3c117054fb924d66c75169ff158c811/tenor.gif?itemid=15471762", "https://media1.tenor.com/images/116fe7ede5b7976920fac3bf8067d42b/tenor.gif?itemid=9200932", "https://media1.tenor.com/images/01a97fee428982b325269207ca22866b/tenor.gif?itemid=16085328", "https://media1.tenor.com/images/291ea37382e1d6cd33349c50a398b6b9/tenor.gif?itemid=10204936", "https://media1.tenor.com/images/5466adf348239fba04c838639525c28a/tenor.gif?itemid=13284057", "https://media1.tenor.com/images/61187dd8c7985c443bf9cd39bc310c02/tenor.gif?itemid=12018805", "https://media1.tenor.com/images/143a887b46092bd880997119ecf09681/tenor.gif?itemid=15177421"]
   embed=discord.Embed(color=0xf4c2c2)
   embed.set_image(url=f"{random.choice(pats)}")
   await ctx.send(f"{member.mention} got a headpat from "+ctx.message.author.mention, embed=embed)
 
-@client.command(aliases=["pedo"])
-async def pedorate(ctx,member : discord.Member):
-  await ctx.send(f"{member.mention} is {random.randint(1,100)}% a pedo")
-
 @client.command()
-async def anthem(ctx):
-  anthems = ["Расцветали яблони и груши, Поплыли туманы над рекой; Выходила на берег Катюша, На высокий берег, на крутой. Выходила, песню заводила Про степного, сизого орла, Про того, которого любила, Про того, чьи письма берегла. Ой, ты песня, песенка девичья, Ты лети за ясным солнцем вслед, И бойцу на дальнем пограничье От Катюши передай привет. Пусть он вспомнит девушку простую, Пусть услышить, как она поет, Пусть он землю бережет родную, А любовь Катюша сбережет. Расщветали яблони и груши, Поплыли туманы над рекой; Выходила на берег Катюша, На высокий берег, на крутой", "Apples and pears were blossoming Mist on the river floating On the bank Katyusha stepped out On the high steep bank Stepped out, started a song About one grey steppe eagle About her loved one Whose letters she cherished Oh song, maiden's song Fly towards the clear sun And to the warrior on a far away border Bring Katyusha's greeting May he remember this simple maiden And hear her singing May he save our motherland And love, Katyusha will save."]
-  embed=discord.Embed(title="the national anthem", description=f"{random.choice(anthems)}", color=0xda0a0a)
-  await ctx.send(embed=embed)
+async def pedo(ctx,member : discord.Member):
+  await ctx.send(f"{member.mention} is {random.randint(1,100)}% a pedo")
 
 @client.command(aliases=["8bal", "9ball"])
 async def _9ball(ctx):
   h = ["its 8ball you donut", "learn to spell its 8ball", "are you retarded, its 8ball"]
   await ctx.send(f"{random.choice(h)}")
-
-@client.command()
-async def ship(ctx, member : discord.Member):
-  await ctx.send(f"{member.mention} loves {ctx.message.author.mention} {random.randint(1,100)}%")
 
 @client.command()
 async def snowy(ctx):
@@ -387,20 +340,6 @@ async def reece(ctx):
   embed=discord.Embed()
   embed.set_image(url="https://res.cloudinary.com/du3fxrdqu/image/upload/v1613907121/peach%20bot/aJ2sccg_erd13r.png")
   await ctx.send(embed=embed)
-  
-@client.command()
-async def petition(ctx):
-  await ctx.send("https://www.change.org/removeMarfromStylis")
-
-@client.command()
-@commands.has_permissions(ban_members=True)
-async def prules(ctx):
-  embed=discord.Embed(color=0xda0a0a)
-  embed.add_field(name="To promote a healthy and organized server, we do require that all of our members abide by the rules. If caught breaking the rules, you can be; a muted from text chats and voice calls, kicked,permanently banned, or in more severe cases, reported to Discord.", value="\n**Rule #1: Respect the community and its members**\nThere is no reason to be outright rude or disrespectful to anyone in this server. Please act civilly and use good judgement.\n\n**Rule #2: No harmful acts to the server or its members**\nHarassment, discriminating other members, and/or threatening the well-being and privacy of other members is strictly prohibited. We want this server to be a clean, friendly environment for all. Phishing, scamming, bullying, spamming, and raiding are also not permitted.\n\n**Rule #3: No NSFW Content**\nAll NSFW content (nudity, intense sexuality, excessive profanity, violence, gore, and any disturbing subject matter) is strictly prohibited on all grounds.\n\n**Rule #4: Use the Discord and its channels accordingly**\nBe sure to utilize each channel for its intended purpose. Don't post anything where it shouldn't be.")
-  await ctx.send(embed=embed)
-  embed=discord.Embed(color=0xda0a0a)
-  embed.add_field(name="Rule #5: No abusing permissions", value="Whether a team member or a fan, please do not abuse your role or admin powers. Doing so will result in instant loss of mod and a potential kick/ban from the server. This also applies to abusing channels and bot commands.\n\n**Note: This server is also in accordance with the Discord Terms of Service and enforces them thoroughly. Anyone caught in violation of the Discord ToS can and will be removed from the server accordingly.**\n\nDiscord Terms of Service: https://discordapp.com/terms\n\nIf you happen to run into any issues while in this server, whether it be a glitch or a server member, please get in contact with a server admin. Thank you!\n\nPermanent Server Invite: https://discord.gg/swnW5GY")
-  await ctx.send(embed=embed)
 
 @client.command()
 async def xeno(ctx):
@@ -418,12 +357,6 @@ async def xeno(ctx):
 @client.command()
 async def sus(ctx,member : discord.Member):
   await ctx.send(f"{member.mention} is {random.randint(1,100)}% sus")
-  
-@client.command()
-async def s(ctx):
-  embed=discord.Embed()
-  embed.add_field(name="hi", value="fuck off пидар 병신")
-  await ctx.send(embed=embed)
 
 @client.command()
 async def avatar(ctx, *, member: discord.Member=None): # set the member object to None
@@ -474,7 +407,7 @@ async def minecraft(ctx, * role: discord.Role):
   await ctx.send("you're now minecrafting")
   
 @client.command()
-async def trouble (ctx):
+async def trouble(ctx):
   embed=discord.Embed()
   embed.set_image(url="https://res.cloudinary.com/du3fxrdqu/image/upload/v1613907283/peach%20bot/gzL8lZY_xm3zry.png")
   await ctx.send(embed=embed)
@@ -506,7 +439,7 @@ async def downbad(ctx,member : discord.Member):
 async def violence(ctx, member: discord.Member):
   embed=discord.Embed(color=0xf4c2c2)
   embed.set_image(url="https://media1.tenor.com/images/92d6db8936ad0924d7f127c54491a737/tenor.gif?itemid=19273167")
-  await ctx.send(f"{member.mention} got violenced by "+ctx.message.author.mention, embed=embed)
+  await ctx.send(f"{member.mention} got violated by "+ctx.message.author.mention, embed=embed)
 
 @client.command(aliases=["whois"])
 async def user(ctx, member: discord.Member = None):
@@ -528,6 +461,42 @@ async def user(ctx, member: discord.Member = None):
     embed.add_field(name="Roles:", value="".join([role.mention for role in member.roles[1:]]))
     embed.add_field(name="Highest Role:", value=member.top_role.mention)
     await ctx.send(embed=embed)
+ 
+@client.command()
+@commands.has_permissions(ban_members=True)
+async def poll(ctx, * , message):
+  embed=discord.Embed(title="cool poll",description=f"{message}")
+  msg=await ctx.channel.send(embed=embed)
+  await msg.add_reaction("✅")
+  await msg.add_reaction("❎")
+
+@client.command()
+async def ky(ctx):
+  embed=discord.Embed()
+  embed.set_image(url="https://res.cloudinary.com/du3fxrdqu/image/upload/v1618330463/peach%20bot/image_2021-04-13_191422_eeu8gy.png")
+  await ctx.send(embed=embed)
+
+@client.command(aliases=["trans"])
+async def trap(ctx,member : discord.Member):
+  await ctx.send(f"{member.mention} is {random.randint(1,100)}% a trap")
+
+@client.command()
+async def reim(ctx):
+  embed=discord.Embed()
+  embed.set_image(url="https://res.cloudinary.com/du3fxrdqu/image/upload/v1618854127/peach%20bot/fwMUbhm_kcq1yl.png")
+  await ctx.send(embed=embed)
+
+@client.command()
+async def punch(ctx, member: discord.Member):
+  embed=discord.Embed(color=0xf4c2c2)
+  embed.set_image(url="https://media1.tenor.com/images/1860c5137c0b4bdc7f1a4e272a624b08/tenor.gif?itemid=21363116")
+  await ctx.send(f"{member.mention} got punched by "+ctx.message.author.mention, embed=embed)
+
+@client.command()
+async def help(ctx):
+ embed=discord.Embed(title="Bot's code: https://github.com/champiix/bot", description="Fun:\n``8ball`` ``dice`` ``coinflip`` ``cough`` ``heal`` ``slap`` ``simp`` ``gay`` ``loli`` ``kill`` ``copypasta`` ``horny`` ``kiss`` ``hug`` ``pat`` ``pedo`` ``sus`` ``women`` ``lesbian`` ``liability`` ``filipino`` ``downbad`` ``violence``\nCustom commands:\n``zach`` ``eesti`` ``suzuka`` ``braindamage`` ``hackerman`` ``alike`` ``mochi`` ``overwatch`` ``champii`` ``scam`` ``snowy`` ``zen`` ``xeno`` ``unimech`` ``ky`` ``punch``\nModeration:\n``clear`` ``kick`` ``ban`` ``mute`` ``unmute``\nInfo/Utility:\n``ping`` ``info`` ``repo`` ``avatar`` ``server`` ``user`` ``poll``", color=0xF2AD7E)
+ embed.set_author(name="Command list",icon_url="https://cdn.discordapp.com/avatars/692360784268754964/8a1a3eb11ceb60d269f3562e3c57ecb2.webp?size=2048")
+ await ctx.send(embed=embed)
 
 keep_alive()
 client.run("TOKEN")
