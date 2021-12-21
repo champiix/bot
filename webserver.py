e51ad3067b4313c58bc749f951dfa6d9a3469a40
champii <37625021+champiix@users.noreply.github.com>
2021-12-21 07:56:07 +0200

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "bro are you disrespecting my japanese culture bro i’m more japan than all of you because i watch anime and you don’t, i bet you don’t even know what Naruto is you degenerate. So before you message me acting like you know my culture maybe check yourself first"
+    return "PEACH COMMANDS<br><br>Fun:<br>8ball, coinflip|cf, copypasta, woman, dice|roll, horny, gay, simp, loli, pedo, sus, liability, lesbian, filipino, hug, hackerman, pat, kiss, slap, kill<br><br>Moderation:<br>ban, clear, mute, kick, unmute, avatar, server<br><br>Custom commands:<br>alike, braindamage, champii, unimech, eesti, ice, manifesto, mochi, miku, pravda, t, snowy, zen, reece, xeno, unimech, trouble<br><br>VPN:<br>pia, nordvpn, tunnelbear<br><br>Bot info:<br>repo, ping, info, help(leads to this website)"
 
 def run():
   app.run(host='0.0.0.0',port=8080)
